28eeba45963f20e8a91f9f901551f033a7aaa743
Brian Petersen <skiwalk3r@gmail.com>
2020-12-03 22:30:28 -0700

    Cleans up old files

--- lib/advent_of_covid.ex
+++ lib/advent_of_covid.ex
@@ -1,18 +1,2 @@
 defmodule AdventOfCovid do
-  @moduledoc """
-  Documentation for `AdventOfCovid`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AdventOfCovid.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/advent_of_covid_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AdventOfCovidTest do
-  use ExUnit.Case
-  doctest AdventOfCovid
-
-  test "greets the world" do
-    assert AdventOfCovid.hello() == :world
-  end
-end
